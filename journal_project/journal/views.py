3ffebf234c8160609200764ff88f568ca567ee0e
brianjkim94 <brianjkim94@gmail.com>
2024-08-07 01:23:08 -0400

    feat: templates/journal

--- journal_project/journal/views.py
+++ journal_project/journal/views.py
@@ -1,3 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import JournalEntry, Date
+from .forms import JournalEntryForm
 
-# Create your views here.
+def entry_list(request):
+    entries = JournalEntry.objects.all()
+    return render(request, 'journal/entry_list.html', {'entries': entries})
+
+def entry_detail(request, pk):
+    entry = get_object_or_404(JournalEntry, pk=pk)
+    return render(request, 'journal/entry_detail.html', {'entry': entry})
+
+def entry_create(request):
+    if request.method == "POST":
+        form = JournalEntryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('entry_list')
+    else:
+        form = JournalEntryForm()
+    return render(request, 'journal/entry_form.html', {'form': form})
+
+def entry_edit(request, pk):
+    entry = get_object_or_404(JournalEntry, pk=pk)
+    if request.method == "POST":
+        form = JournalEntryForm(request.POST, instance=entry)
+        if form.is_valid():
+            form.save()
+            return redirect('entry_detail', pk=pk)
+    else:
+        form = JournalEntryForm(instance=entry)
+    return render(request, 'journal/entry_form.html', {'form': form})
+
+def entry_delete(request, pk):
+    entry = get_object_or_404(JournalEntry, pk=pk)
+    if request.method == "POST":
+        entry.delete()
+        return redirect('entry_list')
+    return render(request, 'journal/entry_confirm_delete.html', {'entry': entry})
+
+def search_by_date(request):
+    if 'date' in request.GET:
+        date_str = request.GET['date']
+        entries = JournalEntry.objects.filter(date__date=date_str)
+    else:
+        entries = JournalEntry.objects.none()
+    return render(request, 'journal/entry_list.html', {'entries': entries})
