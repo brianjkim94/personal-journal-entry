24fd7a91d427a1a965c4e725989efb0dd267a507
brianjkim94 <brianjkim94@gmail.com>
2024-08-10 18:32:00 -0400

    feat: /static/

--- journal_project/settings.py
+++ journal_project/settings.py
@@ -130,7 +130,7 @@ LOGIN_URL = '/login'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
